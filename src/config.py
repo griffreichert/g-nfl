6029db895e01394a5ce5ffba52eb63c378db24b7
Griffin <you@example.com>
2023-10-01 17:59:52 +0100

    pff prop analysis

---
+++ src/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_PATH = Path("../data/")
+PFF_PROP_PATH = DATA_PATH / "pff-props"
+PFF_POWER_PATH = DATA_PATH / "pff-ratings"
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,41 @@ import pandas as pd
 import numpy as np
 import re
 
+nfl_teams = {
+    "ARI",
+    "ATL",
+    "BAL",
+    "BUF",
+    "CAR",
+    "CHI",
+    "CIN",
+    "CLE",
+    "DAL",
+    "DEN",
+    "DET",
+    "GB",
+    "HOU",
+    "IND",
+    "JAX",
+    "KC",
+    "LA",
+    "LAC",
+    "LV",
+    "MIA",
+    "MIN",
+    "NE",
+    "NO",
+    "NYG",
+    "NYJ",
+    "PHI",
+    "PIT",
+    "SEA",
+    "SF",
+    "TB",
+    "TEN",
+    "WAS",
+}
+
 
 def clean_df_columns(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = [
@@ -10,42 +45,65 @@ def clean_df_columns(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def cover_result(row, result_col="result", spread_col="spread_line"):
+    if row[result_col] == row[spread_col]:
+        return 0.5
+    elif row[result_col] < row[spread_col]:
+        return 1
+    return 0
+
+
 def get_nfl_teams() -> list:
     # list(sorted(nfl.import_schedules([2023]).away_team.unique()))
-    return [
-        "ARI",
-        "ATL",
-        "BAL",
-        "BUF",
-        "CAR",
-        "CHI",
-        "CIN",
-        "CLE",
-        "DAL",
-        "DEN",
-        "DET",
-        "GB",
-        "HOU",
-        "IND",
-        "JAX",
-        "KC",
-        "LA",
-        "LAC",
-        "LV",
-        "MIA",
-        "MIN",
-        "NE",
-        "NO",
-        "NYG",
-        "NYJ",
-        "PHI",
-        "PIT",
-        "SEA",
-        "SF",
-        "TB",
-        "TEN",
-        "WAS",
-    ]
+    return list(nfl_teams)
+
+
+def standardize_teams(team):
+    team_map = {
+        "ARZ": "ARI",
+        "BLT": "BAL",
+        "CLV": "CLE",
+        "HST": "HOU",
+        "JAG": "JAX",
+        "JAC": "JAX",
+        "LAR": "LA",
+        "PHL": "PHI",
+        "WSH": "WAS",
+        "WFT": "WAS",
+    }
+    team = team_map.get(team, team)
+    assert team in nfl_teams, team
+    return team
+
+
+def pick_result(row, spread_col="spread_line"):
+    res = 0
+    away_flag = row["pick"] == row["away_team"]
+
+    # determine spread bets
+    if row["pick_type"] in ["bb", "reg", "mnf"]:
+        if away_flag:
+            res = row["away_cover"]
+        else:
+            res = row["home_cover"]
+        # double best bets
+        if row["pick_type"] == "bb":
+            res *= 2
+    # sudden death
+    if row["pick_type"] == "sd":
+        if away_flag:
+            if row["result"] < 0:
+                res = 1
+        elif row["result"] > 0:
+            res = 1
+    if row["pick_type"] == "ud":
+        if away_flag:
+            if row["result"] < 0:
+                res = abs(row[spread_col])
+        else:
+            if row["result"] > 0:
+                res = abs(row[spread_col])
+    return res
 
 
 def reduce_memory_usage(df, verbose=True):
@@ -87,3 +145,61 @@ def reduce_memory_usage(df, verbose=True):
             )
         )
     return df
+
+
+def implied_probability(odds: int, round_n=4) -> float:
+    """Given the odds of a bet, calculate the implied probability
+
+    Args:
+        odds (int): odds of the bet (american so 200 would correspond to a 2/1 bet with 33% implied prob). will convert strings to ints
+        round_n (int, optional): precision of decimal probability. Defaults to 2.
+
+    Returns:
+        float: implied probability of the bet
+    """
+    try:
+        odds = int(odds)
+    except ValueError:
+        print("Invalid odds in betting-utils.py/implied_probability()")
+
+    assert abs(odds) >= 100, "error odds are <100 cannot calculate prob"
+    if odds < 0:
+        return round(odds / (odds - 100), round_n)
+    else:
+        return round(1 - (odds / (odds + 100)), round_n)
+
+
+def odds(implied_prob: float, as_str=False):
+    """Given an implied probability calculate the american odds
+
+    Args:
+        implied_prob (float): implied probability of the bet
+        as_str (bool, optional): will format value as a string and return positive odds with the '+'. Defaults to False.
+
+    Returns:
+        int: american odds of the event, ex: .5 will return 100. '
+    """
+    # favored event, odds will be negative
+    if implied_prob > 0.5:
+        res_odds = int((100 * implied_prob) / (implied_prob - 1))
+        return f"{res_odds}" if as_str else res_odds
+    # underdog event, odds will be positive
+    else:
+        res_odds = int((100 - 100 * implied_prob) / implied_prob)
+        return f"+{res_odds}" if as_str else res_odds
+
+
+def decimal_odds(implied_prob: float) -> float:
+    return 1 / implied_prob
+
+
+def decimal_implied_probability(decimal_odds: float) -> float:
+    return 1 / decimal_odds
+
+
+def decimal_odds_to_american(decimal_odds: float) -> int:
+    if decimal_odds >= 2.0:
+        american_odds = (decimal_odds - 1) * 100
+    else:
+        american_odds = -100 / (decimal_odds - 1)
+    return round(american_odds)
