6fbca71b9ae5e3357d7a70910eb96ef50944b7f6
Griffin Reichert <griffinreichert@gmail.com>
2024-08-24 13:20:42 +0100

    init metric over expectations

--- src/modelling/metrics.py
+++ src/modelling/metrics.py
@@ -1,6 +1,8 @@
-from typing import Literal, Tuple
+from typing import List, Literal, Tuple
 
 import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 success_rate_lambda = lambda x: 1 if x > 0 else 0
 
@@ -46,4 +48,26 @@ def calculate_epa_metrics(
 
 
 def dual_epa_metrics(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """_summary_
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        play by play dataframe
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame]
+        offense epa dataframe, defense epa dataframe
+    """
     return calculate_epa_metrics(data), calculate_epa_metrics(data, "defteam")
+
+
+def metric_over_expectation(
+    data: pd.DataFrame,
+    y_col: str,
+    x_cols: List[str],
+    summary: bool = True,
+    binary: bool = False,
+) -> pd.DataFrame:
+    pass
