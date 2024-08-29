01027a427ec78592df86c0a4123b94c99140f650
Griffin Reichert <griffin.reichert@gmail.com>
2024-08-29 17:39:49 +0100

    add power rating to spread pipeline

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -21,9 +21,42 @@ def get_week_spreads(week: int, season: int = CUR_SEASON) -> pd.DataFrame:
         .query(f"week=={week}")
         .set_index("game_id")
         .drop(columns=["week"])
-        .rename(columns={"spread_line": "market_line"})
+        # .rename(columns={"spread_line": "market_line"})
     )
     return schedule_df
+
+
+def guess_the_lines_ovr(
+    power_df: pd.DataFrame, week: int, season: int = CUR_SEASON
+) -> pd.DataFrame:
+    # get power and schedule_dfs
+    schedule_df = get_week_spreads(week, season)
+
+    gtl = pd.merge(
+        schedule_df, power_df[["net_gpf"]], left_on="away_team", right_index=True
+    ).rename(
+        columns={
+            "net_gpf": "away_gpf",
+        }
+    )
+    gtl = pd.merge(
+        gtl, power_df[["net_gpf"]], left_on="home_team", right_index=True
+    ).rename(
+        columns={
+            "net_gpf": "home_gpf",
+        }
+    )
+    gtl = gtl.drop(columns=["total_line"])
+    gtl["pred_line"] = gtl["home_gpf"] - gtl["away_gpf"] + HFA
+    gtl["difference"] = gtl["pred_line"] - gtl["spread_line"]
+    gtl["pick"] = gtl.apply(
+        lambda row: row["home_team"] if row["difference"] > 0 else row["away_team"],
+        axis=1,
+    )
+    gtl["difference_rank"] = (
+        gtl["difference"].abs().rank(method="dense", ascending=False).astype(int)
+    )
+    return gtl.sort_values("difference_rank")
 
 
 def guess_the_lines(
